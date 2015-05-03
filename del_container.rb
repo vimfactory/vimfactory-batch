affa985c9b8d512fef8e8c69ba1eff9e90da81d6
mosuke5 <w.vamos603@gmail.com>
2015-05-03 04:27:12 -0400

    update

--- del_container.rb
+++ del_container.rb
@@ -3,13 +3,13 @@ require 'docker'
 require 'optparse'
 
 args = {:expire_sec => '7200',
-        :vimrc_path => './', 
+        :vimrc_path => nil,
         :docker_host => 'localhost', 
         :docker_port => '4243',}
 
 OptionParser.new do |opt|
   opt.on('-s=SEC', '--expire-sec', 'default: 7200') { |v| args[:expire_sec] = v }
-  opt.on('-p=PATH', '--vimrc-path', 'default: current directory') { |v| args[:file_path] = v }
+  opt.on('-p=PATH', '--vimrc-path', 'もし指定しなければvimrcは消しません') { |v| args[:vimrc_path] = v }
   opt.on('-H=HOST', '--docker-host', 'default: localhost') { |v| args[:docker_host] = v }
   opt.on('-P=PORT', '--docker-port', 'default: 4243') { |v| args[:docker_port] = v }
   opt.parse!(ARGV)
@@ -33,18 +33,21 @@ containers = Docker::Container.all(opts = {'all' => 1})
 expire_datetime = (Time.now-args[:expire_sec].to_i).to_i
 
 containers.each do |container|
-  if( container.info['Created'] < expire_datetime )
-    container_id = container.info['id'][0,12]
-    del_container = Docker::Container.get(container_id)
-    del_container.stop
-    del_container.delete
-    
-    # 対象コンテナのvimrcファイルがあれば削除
-    if File.exist?("#{args[:vimrc_path]}/vimrc_#{container_id}")
-      File.unlink("#{args[:vimrc_path]}/vimrc_#{container_id}")
-    end
+  next if( container.info['Created'] > expire_datetime )
 
-    logger.info("Delete container(#{container_id}).")
+  container_id = container.info['id'][0,12]
+  del_container = Docker::Container.get(container_id)
+  del_container.stop
+  del_container.delete
+  logger.info("Delete container(#{container_id})")
+  
+  if !args[:vimrc_path].nil?
+    # 対象コンテナのvimrcファイルがあれば削除
+    file = "#{args[:vimrc_path]}/vimrc_#{container_id}"
+    if File.exist?(file)
+      File.unlink(file)
+      logger.info("Delete #{file}")
+    end
   end
 end
 
